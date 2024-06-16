9304ac3c8ad9b7dde1e57f87463b87f5f8a38a7a
rounak161 <142321950+rounak161@users.noreply.github.com>
2024-06-17 00:23:41 +0530

    Describe the changes you made

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
       <div className={classes.wrapper}>
         <div className={classes.left}>
           <Link to='/'>
-            <h2 className={classes.title}>WebDevMania</h2>
+            <h2 className={classes.title}>travello</h2>
           </Link>
         </div>
         <div className={classes.center}>
